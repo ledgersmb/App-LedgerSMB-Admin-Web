6e7dd0ecd1e4c3f7e7175815e0ba6c90ace75a9b
Chris Travers <chris@efficito.com>
2014-10-12 03:45:47 -0700

    removing js debug line

--- public/javascripts/listing.js
+++ public/javascripts/listing.js
@@ -4,7 +4,6 @@ $(document).ready(
      var restore = $("#hdrrestore").text();
      var backup  = $("#hdrbackup").text();
      var reload  = $("#hdrreload").text();
-     window.alert(restore);
      $('a.reload').text('[' + reload + ']');
      $('a.backup').text('[' + backup + ']');
   }
