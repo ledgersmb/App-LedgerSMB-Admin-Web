395bf1e8a26288f00a1f5c140f9b22f9025225df
Chris Travers <chris@efficito.com>
2014-12-09 09:00:10 -0500

    logout path added, but logging out is not working yet

--- lib/App/LedgerSMB/Admin/Web.pm
+++ lib/App/LedgerSMB/Admin/Web.pm
@@ -4,8 +4,7 @@ use Dancer ':syntax';
 
 our $VERSION = '0.1';
 
-get '/' => sub {
-    template 'index';
-};
+get '/' => sub { template 'index'; };
+get '/logout' => sub { template 'logout'; };
 
 true;
